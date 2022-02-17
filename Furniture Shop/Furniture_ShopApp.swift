a7fda4d226674be65aa83ce9157ffd7a8acfe77d
Ahmed Emad <https://github.com/Ahmedemad0>
2022-02-18 00:02:30 +0200

    Initial Commit

---
+++ Furniture Shop/Furniture_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Furniture_ShopApp.swift
+//  Furniture Shop
+//
+//  Created by ahmed on 18/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Furniture_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
